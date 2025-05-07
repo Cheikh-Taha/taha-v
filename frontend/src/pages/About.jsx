f5e2d9f73d523c13e67350c0aeb2c97a46d91d56
akram <bokotos3@gmail.com>
2025-05-07 18:28:03 +0100

    login add

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const About = () => {
   return (
     <div>
-     
+     <h1 className='text-center text-4xl font-bold text-gray-600'>ABOUT US</h1>
+     <div className='my-7 flex flex-col md:flex-row gap-12'>
+      <img className='w-full md:max-w-[600px]' src={assets.about_image}/>
+      <div className='flex flex-row justify-center pt-3'>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur rem dolor mollitia, impedit architecto sint doloribus provident vero rerum beatae dignissimos quas adipisci. Pariatur consequuntur, quisquam nemo iure quasi id?Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur rem dolor mollitia, impedit architecto sint doloribus provident vero rerum beatae dignissimos quas adipisci. Pariatur consequuntur, quisquam nemo iure quasi id?</p>
+      </div>
+     </div>
     </div>
   )
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets';
 
 const Login = () => {
+
+  const [state,setState] = useState('Sign Up');
+
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+  const [name,setName] = useState('');
+  const onSubmitHandler = async(event)=>{
+    event.preventDefault();
+  }
+
   return (
-    <div>
-      
-    </div>
+    <form>
+      <p>hahahh</p>
+      <img src= {assets.signinBackground} />
+     
+    </form>
   )
 }
 
